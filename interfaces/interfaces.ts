06f06fb10884007dcecbdbf30d06c5b206042704
Miguel Figuera <36214292+miguelfiguera@users.noreply.github.com>
2024-02-25 15:46:31 -0400

    add fields date fields for schema models and create interfaces.ts

---
+++ interfaces/interfaces.ts
@@ -0,0 +1,70 @@
+export interface User{
+    id: string
+    userName:string
+    email:string
+    password:string
+    createdAt: Date
+    updatedAt: Date
+    lastSession?: Date
+    role: Role
+    cart:Cart
+    profile: Profile
+}
+
+
+export interface Product{
+    id:number
+    name:string
+    description:string
+    price: number
+    quantity:number
+    inExistence:boolean
+    createdAt: Date
+    updatedAt: Date
+    cart?:Cart[]
+    bills?:Bill[]
+
+}
+
+export interface Cart{
+    id:Number
+    products:Product[]
+    user: User
+    userId:String
+}
+
+export interface Bill{
+    id:Number
+    userEmail:String
+    prodcuts:Product[]
+    amount:Number
+    coordinates: String
+    clientAddress: String
+    paymentMethod: PaymentMethod
+    paymentData:JSON
+    confirmed: Boolean
+    createdAt: Date
+    updatedAt: Date
+}
+
+export interface Profile{
+    id:Number
+    user:User
+    userId:string
+    name:string
+    lastName:string
+    address:string
+    coordinates:string
+}
+
+enum PaymentMethod{
+    PAYPAL,
+    PAGOMOVIL,
+    cashUSD
+  }
+
+  enum Role{
+    Root,
+    Admin,
+    User
+}

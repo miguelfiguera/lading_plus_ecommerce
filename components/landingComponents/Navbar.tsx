3c1a5852ced62bca937536243f60497bfa027189
Miguel Figuera <36214292+miguelfiguera@users.noreply.github.com>
2024-02-29 23:40:05 -0400

    add sticky top to navbar

--- components/landingComponents/Navbar.tsx
+++ components/landingComponents/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top">
       <div className="container-fluid">
         <Link className="navbar-brand border-none" href="#">
           Navbar

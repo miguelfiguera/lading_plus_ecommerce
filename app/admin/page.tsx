5561ccc95631b7949b92fcf5b6b32d97f0a0762a
Miguel Figuera <36214292+miguelfiguera@users.noreply.github.com>
2024-03-19 02:37:42 -0400

    commit to merge

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -16,9 +16,8 @@ export default async function page() {
 
   return (
     <div>
-      {/*       //Cards with number of users, bills (payed and pending), products, monthly revenue.
-
-      //Charts with the number of new users by month
+      {/*
+       //Charts with the number of new users by month
       //chart with the number of bills by month
       //chart with the amount of revenue by month
        */}
